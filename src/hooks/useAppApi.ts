3c8ea0884a7e5e709c35de0b7464f45a6d7bf086
viktorferenczi <ferencziviktor@icloud.com>
2026-02-21 15:21:12 +0100

    https menthod fix

--- src/hooks/useAppApi.ts
+++ src/hooks/useAppApi.ts
@@ -1,4 +1,4 @@
-import { METHOD_TYPES } from "../constants/common/methods";
+import { METHOD_TYPES } from "../constants/common/httpMethods";
 import { AppConfigDTO } from "../dtos/app/dtos";
 import ROUTE from "../routes/route";
 import useApi from "./useApi";
