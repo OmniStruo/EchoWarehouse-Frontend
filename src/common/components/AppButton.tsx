a0c803bcdd00f27f2d3c85ddac05aedcaca67669
viktorferenczi <ferencziviktor@icloud.com>
2026-02-28 16:00:38 +0100

    AppButton - loading state

--- src/common/components/AppButton.tsx
+++ src/common/components/AppButton.tsx
@@ -1,13 +1,20 @@
-const AppButton = ({
-  children,
-  ...props
-}: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
+import { useLoadingContext } from "../../hooks/useLoadingContext";
+import AppSpinner from "./AppSpinner";
+
+interface AppButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  showLoading?: boolean;
+}
+
+const AppButton = ({ children, showLoading, ...props }: AppButtonProps) => {
+  const loader = useLoadingContext();
+
   return (
     <button
       {...props}
+      disabled={props.disabled || loader.loading}
       className={`w-full h-11 bg-primary text-primary-foreground rounded-lg font-semibold text-sm flex items-center justify-center gap-2 hover:opacity-90 transition-opacity shadow-glow mt-2 ${props.className}`}
     >
-      {children}
+      {showLoading && loader.loading ? <AppSpinner size="sm" /> : children}
     </button>
   );
 };
